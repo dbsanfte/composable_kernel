e0ce8f565b9d6df497d68696df785306a0f9a802
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2026-01-13 19:32:43 -0600

    clang-format

--- test/ck_tile/gemm_persistent_async_input/test_gemm_persistent_async_input.cpp
+++ test/ck_tile/gemm_persistent_async_input/test_gemm_persistent_async_input.cpp
@@ -12,10 +12,10 @@
 #include <chrono>
 #include <thread>
 
-using Row = ck_tile::tensor_layout::gemm::RowMajor;
-using Col = ck_tile::tensor_layout::gemm::ColumnMajor;
-using F16 = ck_tile::fp16_t;
-using F32 = ck_tile::fp32_t;
+using Row       = ck_tile::tensor_layout::gemm::RowMajor;
+using Col       = ck_tile::tensor_layout::gemm::ColumnMajor;
+using F16       = ck_tile::fp16_t;
+using F32       = ck_tile::fp32_t;
 using Intrawave = ck_tile::integral_constant<ck_tile::GemmPipelineScheduler,
                                              ck_tile::GemmPipelineScheduler::Intrawave>;
 
@@ -46,8 +46,7 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
     static constexpr ck_tile::index_t K_Warp = 1;
 
     template <bool IsRowMajor>
-    static constexpr ck_tile::index_t get_default_stride(ck_tile::index_t row,
-                                                          ck_tile::index_t col)
+    static constexpr ck_tile::index_t get_default_stride(ck_tile::index_t row, ck_tile::index_t col)
     {
         if constexpr(IsRowMajor)
             return col;
@@ -65,14 +64,14 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
         ck_tile::index_t stride_B = get_default_stride<is_b_row_major>(K, N);
         ck_tile::index_t stride_C = get_default_stride<is_c_row_major>(M, N);
 
-        ck_tile::HostTensor<ADataType> a_m_k(
-            ck_tile::host_tensor_descriptor(M, K, stride_A, ck_tile::bool_constant<is_a_row_major>{}));
-        ck_tile::HostTensor<BDataType> b_k_n(
-            ck_tile::host_tensor_descriptor(K, N, stride_B, ck_tile::bool_constant<is_b_row_major>{}));
-        ck_tile::HostTensor<CDataType> c_m_n_dev_result(
-            ck_tile::host_tensor_descriptor(M, N, stride_C, ck_tile::bool_constant<is_c_row_major>{}));
-        ck_tile::HostTensor<CDataType> c_m_n_host_ref(
-            ck_tile::host_tensor_descriptor(M, N, stride_C, ck_tile::bool_constant<is_c_row_major>{}));
+        ck_tile::HostTensor<ADataType> a_m_k(ck_tile::host_tensor_descriptor(
+            M, K, stride_A, ck_tile::bool_constant<is_a_row_major>{}));
+        ck_tile::HostTensor<BDataType> b_k_n(ck_tile::host_tensor_descriptor(
+            K, N, stride_B, ck_tile::bool_constant<is_b_row_major>{}));
+        ck_tile::HostTensor<CDataType> c_m_n_dev_result(ck_tile::host_tensor_descriptor(
+            M, N, stride_C, ck_tile::bool_constant<is_c_row_major>{}));
+        ck_tile::HostTensor<CDataType> c_m_n_host_ref(ck_tile::host_tensor_descriptor(
+            M, N, stride_C, ck_tile::bool_constant<is_c_row_major>{}));
 
         // Fill input tensors with random values
         ck_tile::FillUniformDistributionIntegerValue<ADataType>{-5, 5, 11939}(a_m_k);
@@ -95,61 +94,82 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
             a_m_k, b_k_n, c_m_n_host_ref);
 
         // Setup kernel configuration for persistent async input GEMM
-        constexpr int kBlockPerCu = 1;
-        constexpr bool kPadM = true;
-        constexpr bool kPadN = true;
-        constexpr bool kPadK = true;
-        constexpr bool DoubleSmemBuffer = true;
-        constexpr bool TransposeC = false;
-        constexpr bool StructuredSparsity = false;
-        constexpr bool Persistent = true;
-        constexpr int NumWaveGroup = 1;
-        constexpr bool Preshuffle = false;
+        constexpr int kBlockPerCu                          = 1;
+        constexpr bool kPadM                               = true;
+        constexpr bool kPadN                               = true;
+        constexpr bool kPadK                               = true;
+        constexpr bool DoubleSmemBuffer                    = true;
+        constexpr bool TransposeC                          = false;
+        constexpr bool StructuredSparsity                  = false;
+        constexpr bool Persistent                          = true;
+        constexpr int NumWaveGroup                         = 1;
+        constexpr bool Preshuffle                          = false;
         constexpr ck_tile::index_t TilePartitionerGroupNum = 8;
-        constexpr ck_tile::index_t TilePartitionerM01 = 4;
+        constexpr ck_tile::index_t TilePartitionerM01      = 4;
 
-        using GemmShape = ck_tile::TileGemmShape<
-            ck_tile::sequence<M_Tile, N_Tile, K_Tile>,
-            ck_tile::sequence<M_Warp, N_Warp, K_Warp>,
-            ck_tile::sequence<M_Warp_Tile, N_Warp_Tile, K_Warp_Tile>>;
+        using GemmShape =
+            ck_tile::TileGemmShape<ck_tile::sequence<M_Tile, N_Tile, K_Tile>,
+                                   ck_tile::sequence<M_Warp, N_Warp, K_Warp>,
+                                   ck_tile::sequence<M_Warp_Tile, N_Warp_Tile, K_Warp_Tile>>;
 
-        using TilePartitioner = ck_tile::GemmSpatiallyLocalTilePartitioner<
-            GemmShape, TilePartitionerGroupNum, TilePartitionerM01>;
+        using TilePartitioner = ck_tile::GemmSpatiallyLocalTilePartitioner<GemmShape,
+                                                                           TilePartitionerGroupNum,
+                                                                           TilePartitionerM01>;
 
-        using GemmUniversalTraits = ck_tile::TileGemmUniversalTraits<
-            kPadM, kPadN, kPadK, DoubleSmemBuffer,
-            ALayout, BLayout, CLayout,
-            TransposeC, StructuredSparsity, Persistent, NumWaveGroup, Preshuffle>;
+        using GemmUniversalTraits = ck_tile::TileGemmUniversalTraits<kPadM,
+                                                                     kPadN,
+                                                                     kPadK,
+                                                                     DoubleSmemBuffer,
+                                                                     ALayout,
+                                                                     BLayout,
+                                                                     CLayout,
+                                                                     TransposeC,
+                                                                     StructuredSparsity,
+                                                                     Persistent,
+                                                                     NumWaveGroup,
+                                                                     Preshuffle>;
 
-        using UniversalGemmProblem = ck_tile::UniversalGemmPipelineProblem<
-            ADataType, BDataType, AccDataType,
-            GemmShape, GemmUniversalTraits, Intrawave::value>;
+        using UniversalGemmProblem = ck_tile::UniversalGemmPipelineProblem<ADataType,
+                                                                           BDataType,
+                                                                           AccDataType,
+                                                                           GemmShape,
+                                                                           GemmUniversalTraits,
+                                                                           Intrawave::value>;
 
         using GemmPipeline = ck_tile::GemmPipelineAgBgCrCompAsync<UniversalGemmProblem>;
 
-        using DsLayout = ck_tile::tuple<>;
+        using DsLayout   = ck_tile::tuple<>;
         using DsDataType = ck_tile::tuple<>;
 
         using GemmEpilogue = ck_tile::CShuffleEpilogue<
-            ck_tile::CShuffleEpilogueProblem<
-                ADataType, BDataType, DsDataType, AccDataType, CDataType,
-                DsLayout, CLayout,
-                ck_tile::element_wise::PassThrough,
-                TilePartitioner::MPerBlock, TilePartitioner::NPerBlock,
-                M_Warp, N_Warp, M_Warp_Tile, N_Warp_Tile, K_Warp_Tile,
-                UniversalGemmProblem::TransposeC,
-                1,     // kNumWaveGroups_
-                false, // FixedVectorSize_
-                1,     // VectorSizeC_
-                false, // TiledMMAPermuteN_
-                1,     // BlockedXDLN_PerWarp_
-                DoubleSmemBuffer>>;
+            ck_tile::CShuffleEpilogueProblem<ADataType,
+                                             BDataType,
+                                             DsDataType,
+                                             AccDataType,
+                                             CDataType,
+                                             DsLayout,
+                                             CLayout,
+                                             ck_tile::element_wise::PassThrough,
+                                             TilePartitioner::MPerBlock,
+                                             TilePartitioner::NPerBlock,
+                                             M_Warp,
+                                             N_Warp,
+                                             M_Warp_Tile,
+                                             N_Warp_Tile,
+                                             K_Warp_Tile,
+                                             UniversalGemmProblem::TransposeC,
+                                             1,     // kNumWaveGroups_
+                                             false, // FixedVectorSize_
+                                             1,     // VectorSizeC_
+                                             false, // TiledMMAPermuteN_
+                                             1,     // BlockedXDLN_PerWarp_
+                                             DoubleSmemBuffer>>;
 
         using Kernel = ck_tile::GemmKernel<TilePartitioner, GemmPipeline, GemmEpilogue>;
 
         // Calculate tiles and chunks for async scheduler
         constexpr ck_tile::index_t tiles_per_chunk = 4;
-        constexpr ck_tile::index_t tile_idx_pivot = 2;  // First 2 tiles don't wait
+        constexpr ck_tile::index_t tile_idx_pivot  = 2; // First 2 tiles don't wait
 
         const ck_tile::index_t tiles_m = ck_tile::integer_divide_ceil(M, M_Tile);
         const ck_tile::index_t tiles_needing_signals =
@@ -165,29 +185,30 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
         // Setup async input scheduler
         ck_tile::PersistentAsyncInputScheduler async_scheduler;
         async_scheduler.tiles_per_chunk_m = tiles_per_chunk;
-        async_scheduler.chunk_signals = d_chunk_signals;
-        async_scheduler.tile_idx_pivot_m = tile_idx_pivot;
+        async_scheduler.chunk_signals     = d_chunk_signals;
+        async_scheduler.tile_idx_pivot_m  = tile_idx_pivot;
 
         // Create UniversalGemmHostArgs with async scheduler
-        ck_tile::UniversalGemmHostArgs<1, 1, 0> host_args(
-            {a_m_k_dev_buf.GetDeviceBuffer()},
-            {b_k_n_dev_buf.GetDeviceBuffer()},
-            {},
-            c_m_n_dev_buf.GetDeviceBuffer(),
-            1,  // k_batch
-            M, N, K,
-            {stride_A},
-            {stride_B},
-            {},
-            stride_C,
-            async_scheduler);
+        ck_tile::UniversalGemmHostArgs<1, 1, 0> host_args({a_m_k_dev_buf.GetDeviceBuffer()},
+                                                          {b_k_n_dev_buf.GetDeviceBuffer()},
+                                                          {},
+                                                          c_m_n_dev_buf.GetDeviceBuffer(),
+                                                          1, // k_batch
+                                                          M,
+                                                          N,
+                                                          K,
+                                                          {stride_A},
+                                                          {stride_B},
+                                                          {},
+                                                          stride_C,
+                                                          async_scheduler);
 
         // Create kernel args using UniversalGemmKernel
         auto kargs = Kernel::UniversalGemmKernel::MakeKernelArgs(host_args);
 
         // Setup grid and blocks for persistent kernel
         ck_tile::stream_config stream_cfg{nullptr, false};
-        const dim3 grids = Kernel::MaxOccupancyGridSize(stream_cfg);
+        const dim3 grids  = Kernel::MaxOccupancyGridSize(stream_cfg);
         const dim3 blocks = Kernel::BlockSize();
 
         if(!Kernel::IsSupportedArgument(kargs))
@@ -203,8 +224,7 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
 
         // Launch kernel
         ck_tile::ignore = ck_tile::launch_kernel(
-            stream_cfg,
-            ck_tile::make_kernel<kBlockPerCu>(Kernel{}, grids, blocks, 0, kargs));
+            stream_cfg, ck_tile::make_kernel<kBlockPerCu>(Kernel{}, grids, blocks, 0, kargs));
 
         // Simulate producer setting chunk signals with interleaved sleep
         // This simulates async input becoming available over time
@@ -235,9 +255,8 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
             *std::max_element(c_m_n_host_ref.mData.begin(), c_m_n_host_ref.mData.end());
 
         const auto rtol = ck_tile::get_relative_threshold<ADataType, CDataType, AccDataType>(K);
-        const auto atol =
-            ck_tile::get_absolute_threshold<ADataType, CDataType, AccDataType>(
-                max_accumulated_value, K);
+        const auto atol = ck_tile::get_absolute_threshold<ADataType, CDataType, AccDataType>(
+            max_accumulated_value, K);
 
         bool pass = ck_tile::check_err(
             c_m_n_dev_result, c_m_n_host_ref, "Error: Incorrect results!", rtol, atol);
@@ -247,35 +266,19 @@ class TestGemmPersistentAsyncInput : public ::testing::Test
 };
 
 // Define test types for different layout combinations
-using RowRowRow_F16F16F32F16 =
-    TestGemmPersistentAsyncInput<Row, Row, Row, F16, F16, F32, F16>;
-using RowColRow_F16F16F32F16 =
-    TestGemmPersistentAsyncInput<Row, Col, Row, F16, F16, F32, F16>;
-using ColRowRow_F16F16F32F16 =
-    TestGemmPersistentAsyncInput<Col, Row, Row, F16, F16, F32, F16>;
-using ColColRow_F16F16F32F16 =
-    TestGemmPersistentAsyncInput<Col, Col, Row, F16, F16, F32, F16>;
+using RowRowRow_F16F16F32F16 = TestGemmPersistentAsyncInput<Row, Row, Row, F16, F16, F32, F16>;
+using RowColRow_F16F16F32F16 = TestGemmPersistentAsyncInput<Row, Col, Row, F16, F16, F32, F16>;
+using ColRowRow_F16F16F32F16 = TestGemmPersistentAsyncInput<Col, Row, Row, F16, F16, F32, F16>;
+using ColColRow_F16F16F32F16 = TestGemmPersistentAsyncInput<Col, Col, Row, F16, F16, F32, F16>;
 
 // Test case for Row-Row-Row layout
-TEST_F(RowRowRow_F16F16F32F16, BasicTest)
-{
-    this->Run();
-}
+TEST_F(RowRowRow_F16F16F32F16, BasicTest) { this->Run(); }
 
 // Test case for Row-Col-Row layout
-TEST_F(RowColRow_F16F16F32F16, BasicTest)
-{
-    this->Run();
-}
+TEST_F(RowColRow_F16F16F32F16, BasicTest) { this->Run(); }
 
 // Test case for Col-Row-Row layout
-TEST_F(ColRowRow_F16F16F32F16, BasicTest)
-{
-    this->Run();
-}
+TEST_F(ColRowRow_F16F16F32F16, BasicTest) { this->Run(); }
 
 // Test case for Col-Col-Row layout
-TEST_F(ColColRow_F16F16F32F16, BasicTest)
-{
-    this->Run();
-}
+TEST_F(ColColRow_F16F16F32F16, BasicTest) { this->Run(); }
